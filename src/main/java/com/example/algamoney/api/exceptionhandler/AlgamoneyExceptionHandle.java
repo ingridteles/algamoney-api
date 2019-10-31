5e226c46f9a6c5a926641519b7d5cc0fae344fd1
Ingrid <ingridteles@gmail.com>
2019-10-31 20:32:58 -0300

    5.4. Validando inconsistências

--- src/main/java/com/example/algamoney/api/exceptionhandler/AlgamoneyExceptionHandle.java
+++ src/main/java/com/example/algamoney/api/exceptionhandler/AlgamoneyExceptionHandle.java
@@ -4,9 +4,13 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
+import javax.validation.ConstraintViolationException;
+
+import org.apache.commons.lang3.exception.ExceptionUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.MessageSource;
 import org.springframework.context.i18n.LocaleContextHolder;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
@@ -36,6 +40,15 @@ public class AlgamoneyExceptionHandle extends ResponseEntityExceptionHandler {
 		List<Erro> erros = Arrays.asList(new Erro(mensagemUsuario, mensagemDesenvolvedor));
 
 		return handleExceptionInternal(ex, erros, headers, HttpStatus.BAD_REQUEST, request);
+	}
+	@ExceptionHandler({ConstraintViolationException.class, DataIntegrityViolationException.class})
+	public ResponseEntity<Object> handleConstraintViolationException(DataIntegrityViolationException ex,  WebRequest request) {
+		String mensagemUsuario = messageSource.getMessage("recurso.operacao-nao-permitida", null,
+				LocaleContextHolder.getLocale());
+		String mensagemDesenvolvedor = ExceptionUtils.getRootCauseMessage(ex);
+		List<Erro> erros = Arrays.asList(new Erro(mensagemUsuario, mensagemDesenvolvedor));
+		return handleExceptionInternal(ex, erros, new HttpHeaders(), HttpStatus.BAD_REQUEST, request);
+		
 	}
 
 	@ExceptionHandler({ EmptyResultDataAccessException.class })
